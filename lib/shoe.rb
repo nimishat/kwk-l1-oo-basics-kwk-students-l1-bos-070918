be13c4d174ee40752b001194e594f8ec62c60f81
nimishat <nimishathakkar83@gmail.com>
2018-07-13 14:12:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 class Shoe
   attr_reader :brand
- attr_accessor :color, :size
+ attr_accessor :color, :size, :material
   
   def initialize(brand)
     @brand = brand
     @color = color
+    @material = material
     @size = size
 end
 end
